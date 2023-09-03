7623d40588358f0b1f6a672a57e9f4eebd7f57fb
bigtorresg <bigtorresg@gmail.com>
2023-09-03 13:21:10 +0300

    send message added

--- chat/urls.py
+++ chat/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ChatRoomsList, ChatRoomDetail
 
 urlpatterns = [
-    path("", ChatRoomsList, name="chat_rooms-list"),
+    path("", ChatRoomsList, name="chat_room-list"),
     path("chat/<int:chat_id>/", ChatRoomDetail, name="chat_room-detail"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import redirect, render, get_object_or_404, get_list_or_404
+from django.urls import reverse
 from .models import ChatRoom, Message
 from django.http import Http404
+from .forms import MessageForm
 
 # Create your views here.
 
@@ -16,8 +18,34 @@ def ChatRoomsList(request):
 def ChatRoomDetail(request, chat_id):
     chat = get_object_or_404(ChatRoom, pk=chat_id)
     messages = get_list_or_404(Message, chat_room=chat)
+    form = MessageForm()
+    if request.method == "POST":
+        return SendMessageView(request, chat, form)
+
     return render(
         request,
         "chat_room_detail.html",
-        context={"chat": chat, "messages": messages},
+        context={"chat": chat, "messages": messages, "form": form},
     )
+
+
+def SendMessageView(request, chat, form):
+    if request.method == "POST":
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.chat_room = chat
+            user.sender = request.user
+            user.save()
+            return redirect("chat_room-detail", chat_id=chat.pk)
+    else:
+        form = MessageForm()
+    return render(request, "chat_room_detail.html", context={"form": form})
+
+
+def EditMessageView(request, chat_id):
+    pass
+
+
+def DeleteMessageView(request, chat_id):
+    pass
